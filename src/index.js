80bf86fe609d727d71fb7ea8956b056b59eda650
S. O <so_933@hotmail.com>
2018-06-19 11:47:19 -0700

    react map

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MapWithASearchBox from "./components/maps";
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(<MapWithASearchBox />, document.getElementById("root"));
