192ff62b1d6afba9370838a323610e310f7c6ca6
S. O <so_933@hotmail.com>
2018-07-03 11:39:02 -0700

    key update

--- src/components/maps.js
+++ src/components/maps.js
@@ -16,7 +16,7 @@ const {
 const MapWithASearchBox = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=&v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=_YOUR_KEY_HERE_&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
